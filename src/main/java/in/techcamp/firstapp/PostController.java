3ddeaf5963ce4bfa94f5b7be8897d1486b1beea4
sun2525 <tooooku88@gmail.com>
2025-04-30 16:51:44 +0900

    ミニアプリ

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,20 +1,39 @@
 package in.techcamp.firstapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
+
     @GetMapping("/hello")
-    public String showHello(Model model) {
+    public String showHello(Model model){
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
     }
 
     @GetMapping
-    public String showList(){
+    public String showList(Model model){
+        var postList = postRepository.findAll();
+        model.addAttribute("postList", postList);
         return "index";
+    }
+
+    @GetMapping("/postForm")
+    public String showPostForm(@ModelAttribute("postForm") PostForm form){
+        return "postForm";
+    }
+
+    @PostMapping("/posts")
+    public String savePost(PostForm form){
+        postRepository.insert(form.getMemo());
+        return "redirect:/";
     }
 }
